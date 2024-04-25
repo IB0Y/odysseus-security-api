925720fc8b8fd9d2c2d25412170abe86ec19c047
CYPHERS <philipwaluisachi@gmail.com>
2024-04-25 18:22:49 +0300

    Setup routes with express

--- controllers/permission.controller.js
+++ controllers/permission.controller.js
@@ -103,3 +103,6 @@ class Permissions {
     }
   }
 }
+
+
+export default new Permissions();
---
+++ routers/permission.router.js
@@ -0,0 +1,7 @@
+const express = require("express");
+
+
+const router = (app) => {
+    const api = "/api/v1/permissions/"
+    app.post(`/`, Permissions.)
+}
