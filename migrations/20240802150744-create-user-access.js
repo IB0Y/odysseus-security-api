ca9c2d67b39ec65652654b40dd045e20c9c0ad9a
crazy8 <philipwaluisachi@gmail.com>
2024-08-02 19:45:04 +0300

    Created user access, user-permission migrations

---
+++ migrations/20240802150744-create-user-access.js
@@ -0,0 +1,39 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('user_access', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      user_email: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      access_token: {
+        type: Sequelize.TEXT,
+      },
+      is_active: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true,
+      },
+      password: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('user_access');
+  }
+};
