78064715968531d43e772779a560ad9e41639b16
crazy8 <philipwaluisachi@gmail.com>
2024-08-04 04:26:57 +0300

    API key middleware created and tested

--- middlewares/apiKey.js
+++ middlewares/apiKey.js
@@ -1,8 +1,33 @@
-export const isAutheticated = async (req, res, next) => {
+const ResponseService = require('../utils/responses');
+
+const database = require('../models');
+const UserAccessModel = database.user_access;
+
+const isAutheticated = async (req, res, next) => {
     const apiKey = req.headers['api-key'];
-    if (apiKey === process.env.API_KEY) {
+
+    if (!apiKey) {
+        return ResponseService.unauthorized({ res });
+    }
+
+    const user = await UserAccessModel.findOne({
+        where: {
+            api_key: apiKey
+        }
+    });
+
+    if (!user) {
+        return ResponseService.unauthorized({ res });
+    }
+
+
+    res.locals.user = user;
+
+    if (user?.is_active) {
         next();
     } else {
         return ResponseService.unauthorized({ res });
     }
 }
+
+module.exports = {isAutheticated};
